ac872c384ece3e3b32cfaf4e851a723e57d0c9fd
Dallin <dallinallred@dhcp-10-5-83-83.ad.uvu.edu>
2024-01-22 18:19:43 -0700

    View stubs

--- ezanalytics-vue/src/router/index.js
+++ ezanalytics-vue/src/router/index.js
@@ -8,14 +8,33 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
-    },
-    {
+    },{
       path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },{
+      path: '/chartsHome',
+      name: 'charts',
+      component: () => import('../views/ChartsHome.vue')
+    },{
+      path: '/chartBuilder',
+      name: 'chartBuilder',
+      component: () => import('../views/ChartBuilder.vue')
+    },{
+      path: '/dashboards',
+      name: 'dashBoards',
+      component: () => import('../views/Dashboards.vue')
+    },{
+      path: '/dbBuilder',
+      name: 'dbBuilder',
+      component: () => import('../views/DBBuilder.vue')
+    },{
+      path: '/users',
+      name: 'users',
+      component: () => import('../views/Users.vue')
     }
   ]
 })
